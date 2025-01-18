17144b91a522d871bf957fa88785c9bb14fb242c
Pradyumn <pradyumnrayasam22@gmail.com>
2025-01-18 07:08:20 +0530

    Updated Player controller

--- Assets/Scripts/Player controll.cs
+++ Assets/Scripts/Player controll.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Timers;
 using Unity.Mathematics;
 using Unity.VisualScripting;
 using UnityEngine;
@@ -16,45 +17,77 @@ public class Playercontroll : MonoBehaviour
     [SerializeField] KeyCode MoveDown;
     [SerializeField] Rigidbody2D rb;
     [SerializeField] float Baseforce;
-    [SerializeField] float maxVelocity;
+    [SerializeField] float maxGroundVelocity = 10;
+    [SerializeField] float maxAirVelocity = 5;
     [SerializeField] float Drag;
     [SerializeField] float jumpForce;
     [SerializeField] float groundCheckDistance;
+
+    public Vector2 velocity { get; private set; }
     float force;
     public bool isGrounded=true;
-    public bool rightFootHit;
-    public bool leftFootHit;
     void Update()
     {
-        force=Baseforce*(maxVelocity-rb.velocity[0]-1);
-        if(Input.GetKey(MoveRight)&&isGrounded&&math.abs(rb.velocity[0])<=math.abs(maxVelocity))
-        {
-            rb.AddForce(Vector2.right*force,ForceMode2D.Force);
-            Debug.Log("Right");
-        }
-        if(Input.GetKeyUp(MoveRight)&&isGrounded)rb.AddForce(Vector2.left*Baseforce,ForceMode2D.Force);
-        if(Input.GetKey(MoveLeft)&&isGrounded&&math.abs(rb.velocity[0])<=maxVelocity)
-        {
-            rb.AddForce(Vector2.left*force,ForceMode2D.Force);
-            Debug.Log("Left");
-        }
-        if(Input.GetKeyUp(MoveRight)&&isGrounded)rb.AddForce(Vector2.left*Baseforce,ForceMode2D.Force);
-        else if(isGrounded)rb.velocity=new Vector2(rb.velocity[0]/Drag,rb.velocity[1]);
-        if(Input.GetKeyDown(jump)&&isGrounded){
-            rb.AddForce(Vector2.up*jumpForce,ForceMode2D.Impulse);
-        }
+        velocity = rb.velocity;
+        float xInput = 0;
+
+        xInput = (Input.GetKey(MoveRight)?1:0) - (Input.GetKey(MoveLeft)?1:0);
         isGrounded=checkforground();
+
+        if(isGrounded)
+        {
+            force=Baseforce*(maxGroundVelocity*xInput-velocity.x);
+            if(xInput > 0 && math.abs(rb.velocity[0])<=math.abs(maxGroundVelocity))
+            {
+                rb.AddForce(Vector2.right*force,ForceMode2D.Force);
+            }
+            else if (xInput < 0 &&math.abs(rb.velocity[0])<=maxGroundVelocity)
+            {
+                rb.AddForce(Vector2.right*force,ForceMode2D.Force);
+            }
+            else
+            {
+                rb.velocity=new Vector2(rb.velocity[0]/Drag,rb.velocity[1]);
+            }
+            if(Input.GetKeyDown(jump)){
+                rb.AddForce(Vector2.up*jumpForce,ForceMode2D.Impulse);
+                Debug.Log("Jumped");
+            }
+        }
+        else
+        {
+            
+            force=Baseforce*(maxAirVelocity*xInput-velocity.x);
+            if(xInput > 0 && math.abs(rb.velocity[0])<=math.abs(maxAirVelocity))
+            {
+                rb.AddForce(Vector2.right*force,ForceMode2D.Force);
+            }
+            else if (xInput < 0 &&math.abs(rb.velocity[0])<=maxAirVelocity)
+            {
+                rb.AddForce(Vector2.right*force,ForceMode2D.Force);
+            }
+            else
+            {
+                rb.velocity=new Vector2(rb.velocity[0]/Drag,rb.velocity[1]);
+            }
+        }
     }
     bool checkforground()
     {
-        RaycastHit2D leftFoot = Physics2D.Raycast(leftFootP.position,-transform.up*groundCheckDistance);
-        RaycastHit2D righFoot = Physics2D.Raycast(rightFootP.position,-transform.up*groundCheckDistance);
+        RaycastHit2D leftFoot = Physics2D.Raycast(leftFootP.position,-transform.up, groundCheckDistance);
         Debug.DrawRay(leftFootP.position,-transform.up*groundCheckDistance);
+
+        RaycastHit2D rightFoot = Physics2D.Raycast(rightFootP.position,-transform.up, groundCheckDistance);
         Debug.DrawRay(rightFootP.position,-transform.up*groundCheckDistance);
-        if(leftFoot&&leftFoot.collider.tag=="Ground"||righFoot&&righFoot.collider.tag=="chain") 
+        
+        if((leftFoot || rightFoot) && (leftFoot.collider.tag == "Ground" || leftFoot.collider.tag == "chain" || rightFoot.collider.tag == "Ground" || rightFoot.collider.tag == "chain" )) 
         {
+            Debug.Log("GG");
             return true;
         }
-        else return false;
+
+        return false;
     }
+
+
 }
